8fbe55e8c545286d8ae7b2b6a9aea0a356d20ab5
Ian Leckey <ian.leckey@gmail.com>
2020-02-25 22:45:16 +0000

    added symphnony cache component as dep. Need to cache jwt for lifetime

--- Epics/Auth.php
+++ Epics/Auth.php
@@ -5,15 +5,15 @@ use Symfony\Component\HttpClient\HttpClient;
 
 class Auth {
 
-	protected $jwt;
+	public $jwt;
 	protected $userId;
-	protected $loginPath = 'login?categoryId=1&gameId=1';
+	protected $endpoint = EPICS__API_ENDPOINT . 'login?categoryId=1&gameId=1';
 	protected $loggedIn;
 
 	public function __construct($username = '', $password = '') {
 		$this->loggedIn = false;
 		$client = HttpClient::create();
-		$response = $client->request('POST', EPICS__API_ENDPOINT . $this->loginPath, [
+		$response = $client->request('POST', $this->endpoint, [
 							'headers' => EPICS__HTTP_HEADERS,
 							'json' => [
 								'email' => (string)$username,
